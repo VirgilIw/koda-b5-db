572ed367f27a05053860c684ef843522f5e083c9
Virgil I Ambar <imanuelambar11@gmail.com>
2026-01-05 22:57:09 +0700

    fix: updated table

--- weekly.sql
+++ weekly.sql
@@ -1,64 +1,117 @@
+CREATE TYPE user_role AS ENUM ('ADMIN', 'USER');
+
 CREATE TABLE users (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    role VARCHAR(10) NOT NULL CHECK (role in ('USER','ADMIN'))
-)
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR,
+  password VARCHAR,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  role user_role,
+  loyalty_point VARCHAR,
+  user_image VARCHAR,
+  last_login TIMESTAMP,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
 
-CREATE Table genre (
-  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  genre_name varchar(255)
-)
+CREATE TABLE genres (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_name VARCHAR
+);
 
-CREATE Table seats (
-  seat_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  seat_info varchar(255) NOT NULL CHECK (seat_info IN ('AVAILABLE', 'SOLD')),
-  seat_price int
-)
+CREATE TABLE actors (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR
+);
 
+CREATE TABLE directors (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  director_name VARCHAR
+);
 
-CREATE Table movie (
-  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  movie_name varchar(255),
-  movie_title varchar(255),
+CREATE TABLE cinema_locations (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  city VARCHAR
+);
+
+CREATE TABLE cinemas (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  cinemas_name VARCHAR,
+  location_id INT,
+  FOREIGN KEY (location_id) REFERENCES cinema_locations (id)
+);
+
+CREATE TYPE seat_type AS ENUM ('single', 'love-love <3');
+
+CREATE TABLE seat_types (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type seat_type,
+  seat_price INT
+);
+
+CREATE TABLE seats (
+  seat_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  seat_info VARCHAR,
+  seat_type_id INT,
+  FOREIGN KEY (seat_type_id) REFERENCES seat_types (id)
+);
+
+CREATE TABLE movies (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  movie_name VARCHAR,
+  movie_title VARCHAR,
   movie_release_date DATE,
-  movie_detail varchar(255),
-  genre_id INT,
-  seat_id INT,
-  FOREIGN KEY (genre_id) REFERENCES genre(id),
-  FOREIGN KEY (seat_id) REFERENCES seats(seat_id)
-)
+  director_id INT,
+  poster_image VARCHAR,
+  backdrop_image VARCHAR,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (director_id) REFERENCES directors (id)
+);
 
-CREATE TABLE orders (
+CREATE TABLE movie_genres (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  order_time timestamp,
-  history_orders timestamp,
-  schedules_id INT,
-  user_id INT,
   movie_id INT,
-  FOREIGN KEY (schedules_id) REFERENCES schedules(id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (movie_id) REFERENCES movie(id)
-)
+  genre_id INT,
+  FOREIGN KEY (movie_id) REFERENCES movies (id),
+  FOREIGN KEY (genre_id) REFERENCES genres (id)
+);
 
-CREATE TABLE studios (
+CREATE TABLE movie_actors (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  studios_name VARCHAR(255),
-  studios_location VARCHAR(255) 
+  movie_id INT,
+  actor_id INT,
+  FOREIGN KEY (movie_id) REFERENCES movies (id),
+  FOREIGN KEY (actor_id) REFERENCES actors (id)
 );
 
 CREATE TABLE schedules (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   movie_id INT,
-  studio_id INT,
-  show_time TIMESTAMP NOT NULL,
-  price INT NOT NULL,
-  FOREIGN KEY (movie_id) REFERENCES movie(id),
-  FOREIGN KEY (studio_id) REFERENCES studios(id)
+  cinema_id INT,
+  show_time TIMESTAMP,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (movie_id) REFERENCES movies (id),
+  FOREIGN KEY (cinema_id) REFERENCES cinemas (id)
 );
 
-SELECT *
--- FROM schedules;
-FROM orders;
+CREATE TABLE payment_methods (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR
+);
 
+CREATE TABLE orders (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_price INT,
+  schedules_id INT,
+  user_id INT,
+  payment_id INT,
+  seats_id INT,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (seats_id) REFERENCES seats (seat_id),
+  FOREIGN KEY (schedules_id) REFERENCES schedules (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (payment_id) REFERENCES payment_methods (id)
+);
